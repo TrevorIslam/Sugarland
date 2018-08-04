bcf4f53b3cdcb6a4afed3fbd55fd96166ac9a330
TrevorIslam <trevorislam@gmail.com>
2018-08-04 14:25:25 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,180 @@
+console.clear();
+var t = 0;
+var mites = [];
+var sugars = [];
+var sc = [];
+var seconds = 1;
+var gridSize = 5;
+var grid = false;
+var highest = 0;
+var lowest;
+var sp;
+var times = 2;
+var st;
+var int;
+var l;
+var sugx;
+var sugy;
+var sugx2;
+var sugy2;
+
+function setup() {
+  createCanvas(500,500);
+  for (let k = 0; k <= 500/gridSize; k++) {
+    sc.push([]);
+    for (let l = 0; l <= 500/gridSize; l++) {
+      sc[k].push(false);
+    }
+  }
+  for (let i = 0; i < 100; i++) {
+    mites.push(new Mite(Math.floor(random(0,(500/gridSize) + 1)),Math.floor(random(0,(500/gridSize) + 1)), 10, color(0,0,0), 1, 1, 1));
+  }
+  for (let i = 0; i < 1500; i++) {
+    sugars.push(new Sugar(Math.floor(random(0,(500/gridSize) + 1)),Math.floor(random(0,(500/gridSize) + 1)), 2));
+    sc[sugars[i].y][sugars[i].x] = true;
+  }
+  highest = mites.length;
+  lowest = mites.length;
+};
+
+function draw () {
+  noStroke();
+  background(150,150,150);
+  for (let m of mites) {
+    m.drawSelf();
+  }
+  for (let s of sugars) {
+    s.drawSelf();
+  }
+  if (mites.length > highest) {
+    highest = mites.length;
+  }
+  if (t%10 === 0) {
+    //console.log(mites.length);
+    repopulate(20);
+    newSugar(times);
+    for (let m of mites) {
+      for (let s of sugars) {
+        if (collision(m,s)) {
+          if (m.show && s.show) {
+            s.changeHealth(-1);
+            m.changeHealth(10);
+          }
+        }
+        if (s.food<=0) {
+          s.show = false;
+          sugars.splice(sugars.indexOf(s), 1);
+        }
+      }
+      if (m.food<=0) {
+        m.show = false;
+        mites.splice(mites.indexOf(m), 1);
+      }
+    }
+    for (let m of mites) {
+      m.move();
+      m.changeHealth(-1 * (m.speed/(m.stamina * (11/24))));
+    }
+  }
+  if (grid) {
+    drawGrid();
+  }
+  if (mites.length < lowest) {
+    lowest = mites.length;
+  }
+  fill(0);
+  textSize(20);
+  text("Highest: " + highest, 10, 460);
+  text("Lowest:  " + lowest, 10, 480);
+  t++;
+};
+
+var repopulate = function(health) {
+  sp = Math.floor(random(0,100));
+  st = Math.floor(random(0,100));
+  int = Math.floor(random(0,100));
+  l = mites.length;
+  for (let i = 0; i < l; i++) {
+    if (mites[i].food >= health) {
+      if (sp < 80) {
+          mites.push(new Mite(mites[i].x,mites[i].y, 10, color(0,0,0), mites[i].speed + 0, mites[i].stamina, mites[i].intel));
+      } else if (sp >= 80 && sp < 91) {
+        if (mites[i].speed < 5) {
+          mites.push(new Mite(mites[i].x,mites[i].y, 10, color(0,0,0), mites[i].speed + 1, mites[i].stamina, mites[i].intel));
+        }
+      } else if (sp >= 91 && sp < 100) {
+        if (mites[i].speed > 1) {
+          mites.push(new Mite(mites[i].x,mites[i].y, 10, color(0,0,0), mites[i].speed - 1, mites[i].stamina, mites[i].intel));
+        }
+      }
+      if (st < 80) {
+          mites.push(new Mite(mites[i].x,mites[i].y, 10, color(0,0,0), mites[i].speed, mites[i].stamina + 0, mites[i].intel));
+      } else if (st >= 80 && st < 91) {
+        if (mites[i].stamina < 5) {
+          mites.push(new Mite(mites[i].x,mites[i].y, 10, color(0,0,0), mites[i].speed, mites[i].stamina + 1, mites[i].intel));
+        }
+      } else if (st >= 91 && st < 100) {
+        if (mites[i].stamina > 1) {
+          mites.push(new Mite(mites[i].x,mites[i].y, 10, color(0,0,0), mites[i].speed, mites[i].stamina - 1, mites[i].intel));
+        }
+      }
+      if (int < 80) {
+          mites.push(new Mite(mites[i].x,mites[i].y, 10, color(0,0,0), mites[i].speed, mites[i].stamina, mites[i].intel + 0));
+      } else if (int >= 80 && int < 91) {
+        if (mites[i].intel < 5) {
+          mites.push(new Mite(mites[i].x,mites[i].y, 10, color(0,0,0), mites[i].speed, mites[i].stamina, mites[i].intel + 1));
+        }
+      } else if (int >= 91 && int < 100) {
+        if (mites[i].intel > 1) {
+          mites.push(new Mite(mites[i].x,mites[i].y, 10, color(0,0,0), mites[i].speed, mites[i].stamina, mites[i].intel - 1));
+        }
+      }
+    }
+  }
+};
+
+var newSugar = function(times) {
+  sugx = Math.floor(random(0,(500/gridSize) + 1));
+  sugy = Math.floor(random(0,(500/gridSize) + 1));
+  sugx2 = Math.floor(random(0,(500/gridSize) + 1));
+  sugy2 = Math.floor(random(0,(500/gridSize) + 1));
+  for (let m of mites) {
+    while ((m.x == sugx && m.y == sugy) || (m.x == sugx2 && m.y == sugy2)) {
+      sugx = Math.floor(random(0,(500/gridSize) + 1));
+      sugy = Math.floor(random(0,(500/gridSize) + 1));
+      sugx2 = Math.floor(random(0,(500/gridSize) + 1));
+      sugy2 = Math.floor(random(0,(500/gridSize) + 1));
+    }
+  }
+  if (times === 1) {
+    sugars.push(new Sugar(sugx,sugy, 5));
+    sc[sugy][sugx] = true;
+  }
+  if (times === 2) {
+    sugars.push(new Sugar(sugx,sugy, 5));
+    sugars.push(new Sugar(sugx2,sugy2, 5));
+    sc[sugy][sugx] = true;
+    sc[sugy2][sugx2] = true;
+  }
+};
+
+var drawGrid = function() {
+  stroke(100,100,200);
+  for (var i = 0; i < 500; i+=gridSize) {
+    line(0,i,500,i);
+    line(i,0,i,500);
+
+    fill(255);
+    //text(i/gridSize, i+gridSize/2, gridSize/2);
+    //text(i/gridSize, gridSize/2, i+gridSize/2);
+  }
+  noStroke();
+};
+
+var collision = function(obj1, obj2){
+  if (obj1.x == obj2.x && obj1.y == obj2.y) {
+    return true;
+  } else {
+    return false;
+  }
+};
---
+++ mite.js
@@ -0,0 +1,192 @@
+class Mite {
+  constructor (x,y,food, color, speed, stamina, intel) {
+    this.x = x;
+    this.y = y;
+    this.food = food;
+    this.show = true;
+    this.color;
+    this.speed = speed;
+    this.stamina = stamina;
+    this.intel = intel;
+  }
+
+  changeHealth(h) {
+    this.food+=h;
+  }
+
+  drawSelf() {
+    if (this.show) {
+      fill((this.speed-1)*51,(this.stamina-1)*51,(this.stamina-1)*51);
+      //console.log(this.stamina)
+
+      rect(this.x*gridSize,this.y*gridSize,gridSize,gridSize);
+    }
+  }
+
+  move() {
+    var r = Math.floor(random(0,100));
+    if (this.x < 501-this.speed && this.x > (this.speed - 1) && this.y < 501 - this.speed && this.y > (this.speed - 1)) {
+      if (this.x + 1 < 500/gridSize && this.x - 1 >= 0 && this.y - 1 >= 0 && this.y + 1 < 500/gridSize) {
+        if (sc[this.y][this.x + 1]) {
+          if (r < (this.intel + 1) * 12.5) {
+            this.x+=this.speed;
+          } else
+          if (r >= (this.intel + 1) * 12.5 && r < (this.intel + 1) * 12.5 + ((100 - (this.intel+1)*12.5)/3)) {
+            this.x-=this.speed;
+          } else
+          if (r >= (this.intel + 1) * 12.5 + ((100 - (this.intel+1)*12.5)/3) && r < (this.intel+1)*12.5 + ((100 - ((this.intel+1)*12.5))*(2/3))) {
+            this.y+=this.speed;
+          } else
+          if (r >= (this.intel+1)*12.5 + ((100 - (this.intel+1)*12.5)*(2/3)) && r < 100) {
+            this.y-=this.speed;
+          }
+        } else
+        if (sc[this.y][this.x - 1]) {
+          if (r < (this.intel + 1) * 12.5) {
+            this.x-=this.speed;
+          } else
+          if (r >= (this.intel + 1) * 12.5 && r < (this.intel + 1) * 12.5 + ((100 - (this.intel+1)*12.5)/3)) {
+            this.x+=this.speed;
+          } else
+          if (r >= (this.intel + 1) * 12.5 + ((100 - (this.intel+1)*12.5)/3) && r < (this.intel+1)*12.5 + ((100 - (this.intel+1)*12.5)*(2/3))) {
+            this.y+=this.speed;
+          } else
+          if (r >= (this.intel+1)*12.5 + ((100 - (this.intel+1)*12.5)*(2/3)) && r < 100) {
+            this.y-=this.speed;
+          }
+        } else
+        if (sc[this.y - 1][this.x]) {
+          if (r < (this.intel + 1) * 12.5) {
+            this.y-=this.speed;
+          } else
+          if (r >= (this.intel + 1) * 12.5 && r < (this.intel + 1) * 12.5 + ((100 - (this.intel+1)*12.5)/3)) {
+            this.x+=this.speed;
+          } else
+          if (r >= (this.intel + 1) * 12.5 + ((100 - (this.intel+1)*12.5)/3) && r < (this.intel+1)*12.5 + ((100 - (this.intel+1)*12.5)*(2/3))) {
+            this.y+=this.speed;
+          } else
+          if (r >= (this.intel+1)*12.5 + ((100 - (this.intel+1)*12.5)*(2/3)) && r < 100) {
+            this.x-=this.speed;
+          }
+        } else
+        if (sc[this.y + 1][this.x]) {
+          if (r < (this.intel + 1) * 12.5) {
+            this.y+=this.speed;
+          }
+          if (r >= (this.intel + 1) * 12.5 && r < (this.intel + 1) * 12.5 + ((100 - (this.intel+1)*12.5)/3)) {
+            this.x+=this.speed;
+          }
+          if (r >= (this.intel + 1) * 12.5 + ((100 - (this.intel+1)*12.5)/3) && r < (this.intel+1)*12.5 + ((100 - (this.intel+1)*12.5)*(2/3))) {
+            this.x-=this.speed;
+          }
+          if (r >= (this.intel+1)*12.5 + ((100 - (this.intel+1)*12.5)*(2/3)) && r < 100) {
+            this.y-=this.speed;
+          }
+        } else
+        {
+          if (r >=  0 && r < 25) {
+            if (this.x < 501-this.speed) {
+              this.x+=this.speed;
+
+            }
+          } else
+          if (r >= 25 && r < 50) {
+            if (this.x > (this.speed - 1)) {
+              this.x-=this.speed;
+            }
+          } else
+          if (r >= 50 && r < 75) {
+            if (this.y < 501 - this.speed) {
+              this.y+=this.speed;
+            }
+          } else
+          if (r >= 75 && r < 100) {
+            if (this.y > (this.speed - 1)) {
+              this.y-=this.speed;
+            }
+          }
+        }
+      }
+    } else
+    {
+      if (r >=  0 && r < 25) {
+        if (this.x < 501-this.speed) {
+          this.x+=this.speed;
+
+        }
+      } else
+      if (r >= 25 && r < 50) {
+        if (this.x > (this.speed - 1)) {
+          this.x-=this.speed;
+
+        }
+      } else
+      if (r >= 50 && r < 75) {
+        if (this.y < 501 - this.speed) {
+          this.y+=this.speed;
+
+        }
+      } else
+      if (r >= 75 && r < 100) {
+        if (this.y > (this.speed - 1)) {
+          this.y-=this.speed;
+
+        }
+      }
+    }
+    // if (r > 25 && r < 51) {
+    //     console.log('x' + this.x);
+    //     console.log('y' + this.y);
+    //   if (this.x > (this.speed - 1)) {
+    //     if (this.x - 1 >= 0) {
+    //       if (sc[this.y][this.x - 1]) {
+    //         this.x-=this.speed;
+    //         console.log('x' + this.x);
+    //       }
+    //     }
+    //   }
+    // }
+    // if (r > 50 && r < 76) {
+    //     console.log('x' + this.x);
+    //     console.log('y' + this.y);
+    //   if (this.y < 501 - this.speed) {
+    //     if (this.y + 1 < 50) {
+    //       if (sc[this.y + 1][this.x]) {
+    //         this.y+=this.speed;
+    //       }
+    //     }
+    //   }
+    // }
+    // if (r > 75 && r < 101) {
+    //     console.log('x' + this.x);
+    //     console.log('y' + this.y);
+    //   if (this.y > (this.speed - 1)) {
+    //     if (this.y - 1 >= 0) {
+    //       if (sc[this.y - 1][this.x]) {
+    //         this.y-=this.speed;
+    //       }
+    //     }
+    //   }
+    // }
+  }
+
+
+}
+
+class Sugar {
+  constructor (x,y,food) {
+    this.x = x;
+    this.y = y;
+    this.show = true;
+    this.food = food;
+  }
+  changeHealth(h) {
+    this.food+=h;
+  }
+  drawSelf() {
+    if (this.show) {
+      fill(255,255,255);
+      rect(this.x * gridSize, this.y * gridSize, gridSize, gridSize);
+    }
+  }
+}
